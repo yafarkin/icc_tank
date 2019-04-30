1c6d41107bf54c129d09b37336980924cdc79b53
Anastasia <tvdlover19@gmail.com>
2019-04-30 15:53:34 +0400

    Добавление классов сущностей
    Добавлены классы Game, Player

---
+++ TankServer/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankServer
+{
+    class Game
+    {
+        public int ID { get; set; }
+        public DateTime Duration { get; set; }
+        public string Winner { get; set; }
+        public List<string> Players { get; set; }
+    }
+}
---
+++ TankServer/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankServer
+{
+    class Player
+    {
+        public int ID { get; set; }
+        public string Tag { get; set; }
+        public string Nickname { get; set; }
+        public decimal MaximumHp { get; set; }
+        public decimal Score { get; set; }
+        public decimal BulletSpeed { get; set; }
+        public decimal Damage { get; set; }
+    }
+}
