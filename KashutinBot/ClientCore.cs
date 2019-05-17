0b7f6238e6745fa92e343b9e397c026c795770b0
Саша Каш <Alex334807@yandex.ru>
2019-05-17 13:08:15 +0400

    Создал бота в отдельном проекте

---
+++ KashutinBot/ClientCore.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TankCommon;
+using TankCommon.Enum;
+using WebSocket4Net;
+
+namespace TankClient
+{
+    public class ClientCore
+    {
+        protected readonly Uri _serverUri;
+        protected readonly string _nickName;
+
+        public ClientCore(string server, string nickname)
+        {
+            _serverUri = new Uri(server);
+            _nickName = nickname;
+        }
+
+        public void Run(bool verbose, Func<int, ServerRequest, ServerResponse> bot, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Подсоединение к серверу {_serverUri} как {_nickName}...");
+
+            using (var wc = new WebSocketProxy(_serverUri))
+            {
+                try
+                {
+                    wc.Open();
+                    var loginResponse = new ServerResponse
+                    {
+                        CommandParameter = _nickName,
+                        ClientCommand = ClientCommandType.Login
+                    };
+                    Console.WriteLine($"{DateTime.Now:T} Логин на сервер как {_nickName}");
+                    wc.Send(loginResponse.ToJson(), cancellationToken);
+                    Console.WriteLine($"{DateTime.Now:T} Логин успешно выполнен.");
+
+                    var tokenSource = new CancellationTokenSource();
+
+                    while (!cancellationToken.IsCancellationRequested && wc.State == WebSocketState.Open)
+                    {
+                        var inputData = wc.GetMessage();
+                        if (string.IsNullOrWhiteSpace(inputData))
+                        {
+                            Thread.Sleep(10);
+                            continue;
+                        }
+
+                        if (verbose)
+                        {
+                            Console.Write(new string(' ', Console.WindowWidth - 1));
+                            Console.CursorLeft = 0;
+                            Console.WriteLine( $"{DateTime.Now:T} Получено состояние от сервера, {inputData.Length} байт");
+                        }
+
+                        var serverRequest = inputData.FromJson<ServerRequest>();
+                        if (serverRequest?.Map == null)
+                        {
+                            continue;
+                        }
+
+                        var serverResponse = bot(wc.MsgCount, serverRequest);
+                        var outputData = serverResponse.ToJson();
+
+                        if (verbose)
+                        {
+                            Console.WriteLine();
+                            Console.Write(new string(' ', Console.WindowWidth - 1));
+                            Console.CursorLeft = 0;
+                            Console.WriteLine($"Сообщений в очереди {wc.MsgCount}");
+
+                            Console.Write(new string(' ', Console.WindowWidth - 1));
+                            Console.CursorLeft = 0;
+                            Console.WriteLine($"{DateTime.Now:T} Передача команды серверу: {serverResponse.ClientCommand}");
+                        }
+
+                        wc.Send(outputData, cancellationToken);
+
+                        if (verbose)
+                        {
+                            Console.Write(new string(' ', Console.WindowWidth - 1));
+                            Console.CursorLeft = 0;
+                            Console.WriteLine($"{DateTime.Now:T} Команда серверу передана.");
+                        }
+                    }
+
+                    if (wc.State != WebSocketState.Open)
+                    {
+
+                        Console.WriteLine($"{DateTime.Now:T} Закрыто соединение с сервером");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{DateTime.Now:T} Запрос на прекращение работы");
+                        var logoutResponse = new ServerResponse
+                        {
+                            ClientCommand = ClientCommandType.Logout
+                        };
+                        var outputData = logoutResponse.ToJson();
+
+                        try
+                        {
+                            wc.Send(outputData, CancellationToken.None);
+                        }
+                        catch
+                        {
+                        }
+
+                        Thread.Sleep(1000);
+                    }
+                    
+                    tokenSource.Cancel();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{DateTime.Now:T} Исключение во время выполнения: {e}");
+                }
+            }
+        }
+    }
+}
---
+++ KashutinBot/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading;
+
+namespace TankClient
+{
+    class Program
+    {
+        public static Queue<ConsoleKey> Keys = new Queue<ConsoleKey>();
+
+        static void Main(string[] args)
+        {
+            var server = ConfigurationManager.AppSettings["server"];
+            var nickname = ConfigurationManager.AppSettings["nickname"];
+
+            Console.WriteLine("Запуск клиента");
+
+            var isSpectator = false;
+
+            Console.WriteLine($"Нажмите 1, что бы подключиться как {nickname}");
+            Console.WriteLine("Нажмите 2, что бы подключиться как наблюдатель");
+            Console.WriteLine("Нажмите 'Esc', что бы выйти");
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.Escape:
+                        return;
+                    case ConsoleKey.D1:
+                        break;
+                    case ConsoleKey.D2:
+                        isSpectator = true;
+                        break;
+                    default:
+                        continue;
+                }
+
+                break;
+            }
+
+            var tokenSource = new CancellationTokenSource();
+            var clientCore = new ClientCore(server, isSpectator ? string.Empty : nickname);
+            var botClass = isSpectator ? new Spectator(tokenSource.Token) as IClientBot : new Bot1();
+            //var botClass = isSpectator ? new Spectator(tokenSource.Token) as IClientBot : new ManualClient();
+
+            var clientThread = new Thread(() => { clientCore.Run(!isSpectator, botClass.Client, tokenSource.Token); });
+            clientThread.Start();
+
+            Console.WriteLine("Нажмите клавишу 'Esc' для завершения работы клиента.");
+            while (clientThread.IsAlive)
+            {
+                Thread.Sleep(100);
+
+                if (Console.KeyAvailable)
+                {
+                    var c = Console.ReadKey(true);
+                    Keys.Enqueue(c.Key);
+
+                    if (c.Key == ConsoleKey.Escape)
+                    {
+                        Keys.Dequeue();
+                        tokenSource.Cancel();
+                    }
+                }
+            }
+
+            Console.WriteLine("Завершение клиента. Нажмите Enter для выхода");
+            Console.ReadLine();
+        }
+    }
+}
