e6d621a61c4230a3ac1968c892f16c94f8ba60d8
Ярослав Добош <snek44@gmail.com>
2019-05-21 15:24:46 +0400

    add enum MapType and class ServerSettings

---
+++ TankServer/ServerSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankServer
+{
+    class ServerSettings
+    {
+        public string SessionName { get; set; }
+        public TankCommon.Enum.MapType MapType { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int MaxClientCount { get; set; }
+    }
+}
