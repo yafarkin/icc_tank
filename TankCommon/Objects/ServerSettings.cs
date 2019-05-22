8567d117802e560f2570181fe67eb400f62382f1
Ярослав Добош <snek44@gmail.com>
2019-05-22 16:17:51 +0400

    dislocation and change classes TankSetting and ServerSetting

---
+++ TankCommon/Objects/ServerSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace TankCommon.Objects
+{
+    public class ServerSettings
+    {
+        [Description("Имя сервера")]
+        public string SessionName { get; set; }
+        [Description("Тип загружаемого шаблона карты")]
+        public Enum.MapType MapType { get; set; }
+        [Description("Ширина генерируемой карты")]
+        public int Width { get; set; } = 20;
+        [Description("Высота генерируемой карты")]
+        public int Height { get; set; } = 20;
+        [Description("Максимальное количество одновременно играющих игроков")]
+        public uint MaxClientCount { get; set; } = 2;
+        [Description("Порт сервера")]
+        public int Port { get; set; } = 1000;
+        [Description("К какому типу игры относится данный сервер")]
+        public Enum.ServerType ServerType { get; set; }
+        [Description("Класс настройки темпа игры")]
+        public TankSettings TankSettings { get; set; } = new TankSettings();
+    }
+}
---
+++ TankCommon/Objects/TankSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using TankCommon.Enum;
+
+namespace TankCommon.Objects
+{    
+    public class TankSettings
+    {
+        [Description("Версия настроек")]
+        public int Version { get; set; }
+        [Description("Скорость игры")]
+        public int GameSpeed { get; set; } = 1;
+        [Description("Множитель скорости танка")]
+        public decimal TankSpeed { get; set; } = 2;
+        [Description("Множитель скорости пули")]
+        public decimal BulletSpeed { get; set; } = 4;
+        [Description("Урон танка")]
+        public decimal TankDamage { get; set; } = 40;
+
+        public TankSettings()
+        {
+            Version++;
+        }
+
+        public void UpdateAll(int gameSpeed, decimal tankSpeed, decimal bulletSpeed, decimal tankDamage)
+        {
+            Version++;
+            GameSpeed = gameSpeed;
+            TankSpeed = tankSpeed;
+            BulletSpeed = bulletSpeed;
+            TankDamage = tankDamage;
+        }
+    }
+}
