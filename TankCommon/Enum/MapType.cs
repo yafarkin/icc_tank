bf94eb363c8d5378cf47d99c275a936162efdba1
Alexandr <alex334807@yandex.ru>
2019-05-28 22:57:02 +0400

    Дополнил в список карт, Добавил три файла мануальных карты.

--- TankCommon/Enum/MapType.cs
+++ TankCommon/Enum/MapType.cs
@@ -2,7 +2,11 @@
 {
     public enum MapType
     {
-        Base,
-        Base2
+        Default_map,
+        Emty_map,
+        Water_map,
+        Manual_Map_1,
+        Manual_Map_2,
+        Manual_Map_3
     }
 }
