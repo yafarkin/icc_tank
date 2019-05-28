ff353bc56f849ffbd46a824ba56dd8f431ed20d3
Anastasia <tvdlover19@gmail.com>
2019-05-28 09:59:42 +0400

    Изменение ISettings, TankSettings и ServerSettings
    Обновлены классы и интерфейс настроек

--- TankCommon/ISettings.cs
+++ TankCommon/ISettings.cs
@@ -1,13 +1,48 @@
 ﻿using System;
 using TankCommon.Enum;
+using System.ComponentModel;
 
 namespace TankCommon
-{    
+{
     interface ISettings
     {
-        string ServerName { get; set; }
-        ServerType ServerType { get; set; }
-        TimeSpan SessionTime { get; set; }
-        decimal GameSpeed { get; set; }
+        [Description("Скорость игры")]
+        int GameSpeed { get; set; }
+
+        [Description("Время действия бонусов (ms)")]
+        int TimeOfActionUpgrades { get; set; }
+
+        [Description("Шанс появления бонусов")]
+        double ChanceSpawnUpgrades { get; set; }
+
+        [Description("Скорость танков")]
+        decimal TankSpeed { get; set; } 
+
+        [Description("Скорость пуль")]
+        decimal BulletSpeed { get; set; } 
+
+        [Description("Урон танков")]
+        decimal TankDamage { get; set; } 
+
+        [Description("Максимальный HP танков")]
+        int TankMaxHP { get; set; } 
+
+        [Description("Показатели бонуса увеличения скорости пуль")]
+        int IncreaseBulletSpeed { get; set; } 
+
+        [Description("Показатели бонуса увеличения урона танков")]
+        int IncreaseDamage { get; set; }
+
+        [Description("Показатели бонуса лечения")]
+        int RestHP { get; set; } 
+
+        [Description("Показатели бонуса неуязвимости (ms)")]
+        int TimeOfInvulnerability { get; set; } 
+
+        [Description("Показатели бонуса увеличения максимального количества HP")]
+        int IncreaseHP { get; set; }
+
+        [Description("Показатели бонуса увеличения скорости танка")]
+        int IncreaseSpeed { get; set; }
     }
 }
