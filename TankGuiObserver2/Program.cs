5604ff914700b7983a002eef6f0b53bc2e145243
Vladimir Efremov <VladimirEfremovPMIb1502@yandex.com>
2019-06-04 16:49:22 +0400

    Последний комит за сегодня

--- TankGuiObserver2/Program.cs
+++ TankGuiObserver2/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace TankGuiObserver2
+﻿#define Sandbox_0
+
+using System;
+using System.Xml;
+
+namespace TankGuiObserver2
 {
     class Program
     {
@@ -7,6 +12,7 @@
              */
         static void Main(string[] args)
         {
+#if Sandbox_0
             System.Drawing.Size screenSize = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size;
             using (Game game = new Game("Battle city v0.1",
                 screenSize.Width, 
@@ -14,6 +20,39 @@
             {
                 game.RunGame();
             }
+#else
+            string neadedValue = "";
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("TankGuiObserver2.exe.config");
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xRoot)
+            {
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "add")
+                    {
+                        xDoc.WriteTo(null);
+                        string data_to_parse = childnode.OuterXml;
+                        if (childnode.OuterXml.Contains("ws"))
+                        {
+                            string[] splitted = data_to_parse.Split('"');
+                            foreach (var s in splitted)
+                            {
+                                if (s.Contains("ws"))
+                                {
+                                    neadedValue = s;
+                                }
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("BLALA: ", neadedValue.Equals("ws://10.22.2.120:2000"));
+            Console.Read();
+#endif
         }
     }
 }
